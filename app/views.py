1f5da7efa7b5f1aff41a0b69727c05e7a7705ed1
lukaszjanyga <alvarglj@gmail.com>
2017-01-26 09:26:36 +0100

    display posts in author page

--- app/views.py
+++ app/views.py
@@ -98,7 +98,19 @@ def post(request, post_id):
 def author(request, author_id):
     try:
         author = Author.objects.get(id=author_id)
-        return render(request, 'author.html', {"author": author})
+        posts = author.post_set.all()
+        paginator = Paginator(posts, 10)  # Show 10 contacts per page
+        page = request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
+        return render(request, 'author.html', {"author": author, "posts": posts})
     except Post.DoesNotExist:
         return index(request)
 

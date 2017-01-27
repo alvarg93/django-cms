8418c8c283ba25895dbadfa8c848eec1b3fbfcfa
lukaszjanyga <alvarglj@gmail.com>
2017-01-27 19:39:11 +0100

    remove unused views

--- janyga/urls.py
+++ janyga/urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework import routers
 
 from app import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'posts', views.PostViewSet)
-
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
     url('', include('registration.backends.default.urls'), name="auth"),
     url('', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
